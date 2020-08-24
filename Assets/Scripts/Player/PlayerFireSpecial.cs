b96c76193eb0fe25c24304a1e708449d8a9c2180
Liliane Castro <lcs2@icomp.ufam.edu.br>
2020-08-23 21:53:18 -0400

    feat(PlayerFireSpecial): player can jump and shoot down to jump higher

---
+++ Assets/Scripts/Player/PlayerFireSpecial.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFireSpecial : MonoBehaviour
+{ 
+    public Transform        posSpawnSpecial;
+    public float            forcePlayerImpulse;
+    public float            speedShot;
+
+    private Rigidbody2D     playerRb;
+    private bool            isSpecialUse;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetButton("Fire2")  && Input.GetKeyDown(KeyCode.S) && !isSpecialUse)
+        {
+            isSpecialUse = true;
+
+            GameObject shotSpecial = Instantiate(GameControllerFire.Instance.shotPrefab, posSpawnSpecial);
+            shotSpecial.TryGetComponent(out Rigidbody2D shotRb);
+
+            shotRb.AddForce(Vector2.down * speedShot, ForceMode2D.Impulse);
+            playerRb.AddForce(Vector2.up * forcePlayerImpulse, ForceMode2D.Force);
+            StartCoroutine("DelaySpecialToUseAgain");
+        }
+    }
+
+    IEnumerator DelaySpecialToUseAgain()
+    {
+        yield return new WaitForSeconds(1f);
+        isSpecialUse = false;
+    }
+}
