610992b37aaedd8af64c52000f81469d1698881b
Liliane Castro <lcs2@icomp.ufam.edu.br>
2021-02-08 17:28:40 -0400

    feat(Shot): update destroy

--- Assets/Scripts/Player/PlayerFireSpecial.cs
+++ Assets/Scripts/Player/PlayerFireSpecial.cs
@@ -20,14 +20,15 @@ public class PlayerFireSpecial : MonoBehaviour
 
             GameObject shotSpecial = Instantiate(GameController.Instance.shotSpecialPrefab, posSpawnSpecial.position, posSpawnSpecial.rotation);
             
-            StartCoroutine("DelaySpecialToUseAgain");
+            StartCoroutine(DelaySpecialToUseAgain(shotSpecial));
         }
     }
 
-    private IEnumerator DelaySpecialToUseAgain()
+    private IEnumerator DelaySpecialToUseAgain(GameObject shotSpecial)
     {
         SoundManager.Instance.playFx(0);
         yield return new WaitForSeconds(timeToUseSpecialAgain);
+
         isSpecialUse = false;
     }
 }
--- Assets/Scripts/Player/Shot.cs
+++ Assets/Scripts/Player/Shot.cs
@@ -1,7 +1,14 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Shot : MonoBehaviour
 {
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(4.0f);
+        Destroy(this.gameObject);
+    }
+    
     private void OnBecameInvisible() 
     {
         Destroy(this.gameObject);
